3f0cb8624751dcab1eb046a7959e9074bb5b4bd6
HaoPing0124 <haoping0124@gmail.com>
2026-07-22 22:42:05 +0800

    feat(compiler): implement source compilation with error redirection

---
+++ common/util.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+namespace ns_util
+{
+    // 路径工具类
+    const std::string temp_path = "./temp/";
+    class PathUtil
+    {
+    public:
+        static std::string AddSuffix(const std::string &file_name, const std::string &suffix)
+        {
+            std::string path_name = temp_path;
+            path_name += file_name;
+            path_name += suffix;
+            return path_name;
+        }
+
+        // 编译时需要有的临时文件
+        // 构建源文件路径+后缀的完整文件名
+        // 1234 -> ./temp/1234.cpp
+        static std::string Src(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".cpp");
+        }
+
+        static std::string Exe(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".exe");
+        }
+
+        static std::string CompilerError(const std::string &file_name)
+        {
+            return AddSuffix(file_name, ".compile_error");
+        }
+    };
+
+    // 文件工具类
+    class FileUtil
+    {
+    public:
+        static bool IsFileExists(const std::string &path_name)
+        {
+            struct stat st;
+            if (stat(path_name.c_str(), &st) == 0)
+            {
+                // 获取属性成功，文件已经存在
+                return true;
+            }
+
+            return false;
+        }
+    };
+}

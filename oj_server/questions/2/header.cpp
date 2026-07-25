2e0029491a8dda3201070111afce924bb71fdb3e
HaoPing0124 <haoping0124@gmail.com>
2026-07-25 21:20:27 +0800

    feat(oj-server): add initial question bank with metadata and test cases

---
+++ oj_server/questions/2/header.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int Max(const vector<int> &v)
+    {
+        // write code here
+    }
+};

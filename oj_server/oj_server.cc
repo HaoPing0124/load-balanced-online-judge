f847b347d85a2cf05187935c7b7bc567a8811eda
HaoPing0124 <haoping0124@gmail.com>
2026-07-28 23:36:27 +0800

    feat(oj-server): wire question and judge routes to control layer

--- oj_server/oj_server.cc
+++ oj_server/oj_server.cc
@@ -34,10 +34,14 @@ int main()
                 resp.set_content(html, "text/html; charset=utf-8"); });
 
     // 用户提交代码，使用判题功能(1. 每道题的测试用例 2. compile_and_run)
-    svr.Get(R"(/judge/(\d+))", [](const Request &req, Response &resp)
-            {
-        std::string number = req.matches[1];
-        resp.set_content("第" + number + "题的判题", "text/plain; charset=utf-8"); });
+    svr.Post(R"(/judge/(\d+))", [&ctrl](const Request &req, Response &resp)
+             {
+                 std::string number = req.matches[1];
+                 std::string result_json;
+                 ctrl.Judge(number, req.body, &result_json);
+                 resp.set_content(result_json, "application/json;charset=utf-8");
+                 // resp.set_content("指定题目的判题: " + number, "text/plain; charset=utf-8");
+             });
 
     svr.set_base_dir("./wwwroot");
     svr.listen("0.0.0.0", 8085);

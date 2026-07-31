e5f818ce52bb5b1f3b3396afc95379f7d07bf210
HaoPing0124 <haoping0124@gmail.com>
2026-07-31 20:56:03 +0800

    fix(compiler): prevent duplicate header inclusion in online judging

--- compile_server/compiler.hpp
+++ compile_server/compiler.hpp
@@ -58,7 +58,7 @@ namespace ns_compiler
                 // 子进程: 调用编译器，完成对代码的编译工作
                 // g++ -o target src -std=c++11
                 execlp("g++", "g++", "-o", PathUtil::Exe(file_name).c_str(),
-                       PathUtil::Src(file_name).c_str(), "-std=c++11", nullptr);
+                       PathUtil::Src(file_name).c_str(), "-D", "COMPILER_ONLINE", "-std=c++11", nullptr);
 
                 LOG(ERROR) << "启动编译器 g++ 失败，可能是参数错误" << "\n";
                 exit(2);
